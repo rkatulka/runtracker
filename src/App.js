28fbf11bd58a2e8d26c31bc0e56ebac83ff6364b
David Aber <aberdb@dukes.jmu.edu>
2018-10-08 17:58:57 -0400

    Initial skeleton for UI

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2> Currently under development! </h2>
+          <p> Please come back soon. :) </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
